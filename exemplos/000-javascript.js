c77f3f277c62d121fe27bb0b84844ddadf1ae3e2
ganhadores <124818763+ganhadores@users.noreply.github.com>
2023-02-15 17:30:07 -0300

    variáveis

--- exemplos/000-javascript.js
+++ exemplos/000-javascript.js
@@ -1,1 +1,33 @@
-console.log("Olá estou programando em javascript");
+// o comando abaixo apresenta uma mensagem na tela 
+console.log("Olá estou programando em javascript"); 
+
+/*
+    comentário que ocupa mais
+    de uma linha
+
+*/
+
+// criação de variável do tipo texto 
+let nome = 'Aysla Beatriz Novais';
+// criação de variável do tipo numérico (inteiro)
+let idade = 17;
+// criação de variavél do tipo numérico (decimal)
+let altura = 1.70;
+// criação de variavél do tipo verdadeiro/falso
+let trabalha = true; // true = verdadeiro | false = falso
+// exemplo de variavél de nome composto
+let corFavorita = 'Vermelho';
+// exemplo de variavél do tipo lista de valores
+let cores_favoritas = ['Vermelho', 'Branco', 'Preto'];
+// exemplo de variavél que armazena um objeto 
+let endereco = {
+  'logradouro'    :  'Rua',
+  'nome_logradouro'  :   'Brasil',
+  'número'     : '50 A',
+  'bairro'    :  'Centro',
+  'cidade'   :   'Tupã'   
+
+};
+let covid;
+let dengue = null;
+
