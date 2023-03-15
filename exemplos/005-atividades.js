26e214dfc223d90fb23a888c4e3189310164b17b
ganhadores <124818763+ganhadores@users.noreply.github.com>
2023-03-15 17:30:35 -0300

    aula 3- 15/03

---
+++ exemplos/005-atividades.js
@@ -0,0 +1,10 @@
+/*
+Crie um programa que receba o nome e a nota de 4 matérias
+Exibir o nome das 4 matérias com  a nota e a média final.
+Ex:
+Matemática: 7,00
+Ingles: 10,00
+Geografia: 8,00
+Fisica: 8,00
+Média: 8,25
+/* 
