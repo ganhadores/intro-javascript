2736164db00d2c81877c0d9e21f6856b65efa4b7
ganhadores <124818763+ganhadores@users.noreply.github.com>
2023-03-22 17:28:15 -0300

    aula 22
    atividade 5

--- exemplos/005-atividades.js
+++ exemplos/005-atividades.js
@@ -7,4 +7,29 @@ Ingles: 10,00
 Geografia: 8,00
 Fisica: 8,00
 Média: 8,25
-/* 
+*/
+
+let entrada = require('prompt-sync')();
+console.log ('Programa que receba a nota de 4 matérias')
+let mat1 = entrada('Digite o nome da primeira matéria: ')
+let n1 = parseFloat(entrada('Digite a nota de ' + mat1 + ': '));
+
+let mat2 = entrada('Digite o nome da segunda materia: ')
+let n2 = parseFloat(entrada('Digite a nota de' + mat2 + ': '));
+
+let mat3 = entrada('Digite o nome da terceira materia: ')
+let n3 = parseFloat(entrada('Digite a nota de ' + mat3 + ': '));
+
+let mat4 = entrada('Digite o nome da quarta materia: ')
+let n4 = parseFloat(entrada('Digite a nota da ' + mat4 + ': '));
+
+let media = (n1 + n2 + n3 + n4) / 4;
+
+console.log('Notas: ');
+console.log(mat1 + ': ' + n1.toFixed(2));
+console.log(mat1 + ': ' + n2.toFixed(2));
+console.log(mat1 + ': ' + n3.toFixed(2));
+console.log(mat1 + ': ' + n4.toFixed(2));
+console.log('Media: ' + media.toFixed(2));
+
+
