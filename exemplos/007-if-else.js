30718279e7923b778ea7d7dbef49d59e897906e7
ganhadores <124818763+ganhadores@users.noreply.github.com>
2023-03-29 17:30:34 -0300

    aula 7

---
+++ exemplos/007-if-else.js
@@ -0,0 +1,14 @@
+// Estruturas condicionais
+
+const entrada = require ('prompt-sync') ();
+
+let nome = entrada('Digite seu nome: ');
+let letra = nome.charA(0).toLocaleLowerCase;
+
+let vogais = /[aeiou]/; //regex
+
+if (vogais.test(letra)) {
+    console.log('A 1 letra do seu nome é uma vogal! ');
+} else {
+    console.log('A 1 letra do seu nome é uma consoante! ');
+}
